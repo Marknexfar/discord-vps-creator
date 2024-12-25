09629ee819e131508dcf79213e454532f991a317
Marknexfar <mongdautienganh@gmail.com>
2024-12-25 23:37:23 +0700

    Create c.py

---
+++ c.py
@@ -0,0 +1,12 @@
+import gdown
+
+# URL file Google Drive
+url = "https://archive.org/download/en-us_windows_10_consumer_editions_version_22h2_updated_feb_2023_x64_dvd_c29e4bb3/en-us_windows_10_consumer_editions_version_22h2_updated_feb_2023_x64_dvd_c29e4bb3.iso"
+
+# Đường dẫn lưu file sau khi tải
+output = "/mnt/win10.zip"  # Đổi tên file tùy ý
+
+# Tải file
+gdown.download(url, output, quiet=False)
+
+print(f"File đã được tải về và lưu tại: {output}")
