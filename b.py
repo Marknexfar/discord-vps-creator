ccb84b73900c7656235e8be10050a033f8492a69
Marknexfar <mongdautienganh@gmail.com>
2024-12-26 19:50:00 +0700

    Update b.py

--- b.py
+++ b.py
@@ -1,7 +1,7 @@
 import gdown
 
 # URL file Google Drive (thay đổi từ dạng "view" sang "uc" để gdown hoạt động)
-url = "https://drive.google.com/uc?id=11reY6uzf0BdTLLbCj_ecs0qDxWFaLTEx"
+url = "https://drive.usercontent.google.com/download?id=11reY6uzf0BdTLLbCj_ecs0qDxWFaLTEx&authuser=0"
 
 # Đường dẫn lưu file sau khi tải
 output = "/mnt/win11.zip"  # Đường dẫn đầy đủ nơi lưu file
